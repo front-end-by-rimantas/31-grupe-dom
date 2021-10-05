c51217e722ed8f2a41dfd99c1e654571c0892606
Rimantas <belauzas@yahoo.com>
2021-10-05 19:23:32 +0300

    Solution 6

--- 06/main.js
+++ 06/main.js
@@ -1,6 +1,8 @@
 const logoDOM = document.querySelector('.logo');
 const hamburgerDOM = document.querySelector('.hamburger');
+const navDOM = document.querySelector('nav');
 
 hamburgerDOM.addEventListener('click', function () {
     logoDOM.classList.toggle('big');
+    navDOM.classList.toggle('visible');
 });
